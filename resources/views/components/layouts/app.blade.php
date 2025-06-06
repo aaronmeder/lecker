b6af4ebfbf5cb03dfd2ec425150c2dbed548678a
Aaron Meder <meder@soda.today>
2025-06-06 16:08:18 +0200

    update

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,52 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
-    <script>
-        window.setAppearance = function(appearance) {
-            let setDark = () => document.documentElement.classList.add('dark')
-            let setLight = () => document.documentElement.classList.remove('dark')
-            let setButtons = (appearance) => {
-                document.querySelectorAll('button[onclick^="setAppearance"]').forEach((button) => {
-                    button.setAttribute('aria-pressed', String(appearance === button.value))
-                })
-            }
-            if (appearance === 'system') {
-                let media = window.matchMedia('(prefers-color-scheme: dark)')
-                window.localStorage.removeItem('appearance')
-                media.matches ? setDark() : setLight()
-            } else if (appearance === 'dark') {
-                window.localStorage.setItem('appearance', 'dark')
-                setDark()
-            } else if (appearance === 'light') {
-                window.localStorage.setItem('appearance', 'light')
-                setLight()
-            }
-            if (document.readyState === 'complete') {
-                setButtons(appearance)
-            } else {
-                document.addEventListener("DOMContentLoaded", () => setButtons(appearance))
-            }
-        }
-        window.setAppearance(window.localStorage.getItem('appearance') || 'system')
-    </script>
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+   <!--  <link rel="stylesheet" href="{{ mix('css/app.css') }}"> -->
+    
+   @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 
-<body class="bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200 antialiased" x-data="{
-    sidebarOpen: localStorage.getItem('sidebarOpen') === null ? window.innerWidth >= 1024 : localStorage.getItem('sidebarOpen') === 'true',
-    toggleSidebar() { 
-        this.sidebarOpen = !this.sidebarOpen;
-        localStorage.setItem('sidebarOpen', this.sidebarOpen);
-    },
-    temporarilyOpenSidebar() {
-        if (!this.sidebarOpen) {
-            this.sidebarOpen = true;
-            localStorage.setItem('sidebarOpen', true);
-        }
-    },
-    formSubmitted: false,
-}">
+<body>
 
     <!-- Main Container -->
     <div class="min-h-screen flex flex-col">
